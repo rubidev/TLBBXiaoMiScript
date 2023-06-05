3defe6d641115fbe534fd433f1e6f5ad7a2ef46d
ljtyllr <601039274@qq.com>
2023-06-05 14:29:25 +0800

    fix | 修复乘风礼盒未全部开启

--- 可release脚本/开雅趣乘风礼盒.lua
+++ 可release脚本/开雅趣乘风礼盒.lua
@@ -1,9 +1,9 @@
 
 function OpenGift()
     右键使用物品("雅趣乘风礼盒")
-    延时(500)
+    延时(1000)
     LUA_Call([[setmetatable(_G, {__index = SpecialGift_ThreeChoiceTongyong_Env});SpecialGift_ThreeChoiceTongyong_Choose(3)]])  -- 选中第三个，瑶华璧
-    延时(500)
+    延时(1000)
     LUA_Call([[setmetatable(_G, {__index = SpecialGift_ThreeChoiceTongyong_Env});SpecialGift_ThreeChoiceTongyong_GetClick()]])
 end
 
@@ -11,10 +11,12 @@ end
 function main()
     屏幕提示("【雨夜出品, 必是精品】使用背包中的雅趣乘风礼盒, 选择瑶华璧")
     local giftCount = tonumber(获取背包物品数量("雅趣乘风礼盒"))
-	
-	for i = 1,giftCount do
+
+    local index = 0
+    while index < giftCount do
 	    OpenGift()
-		延时(1000)
+        index = index + 1
+		延时(2000)
 	end
 
 end
