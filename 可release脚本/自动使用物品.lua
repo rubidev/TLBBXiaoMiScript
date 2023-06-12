4b7cb35502ddaeb1609a580504f25f3b12582cd0
Your Name <you@example.com>
2023-06-12 20:59:57 +0800

    update | release 自动使用物品脚本

---
+++ 可release脚本/自动使用物品.lua
@@ -0,0 +1,33 @@
+function FirstUse()
+    -- 第一轮使用，打开礼盒
+    local pkgCount = LUA_取返回值([[return GetActionNum("packageitem");]])
+    local patternList = {'真元·.+符', '归宁澄心华盒', '静心雅盒', '.+运功典藏'}
+    for i = 1, tonumber(pkgCount) do
+        local pkgItem = LUA_取返回值(string.format([[
+            theAction,bLocked = PlayerPackage:EnumItem("base", %d - 1);  -- szPacketName = "base"、"material"、"quest"
+            if theAction:GetID() ~= 0 then
+                return theAction:GetName();
+            end
+            return -1
+        ]], i))
+
+        if pkgItem ~= -1 then
+            for k, pattern in pairs(patternList) do
+                if string.find(pkgItem, pattern) then
+                    -- 真元·xx符、归宁澄心华盒、静心雅盒、任脉运功典藏
+                    while true do
+                        if tonumber(获取背包物品数量(pkgItem)) <= 0 then
+                            break
+                        end
+                        右键使用物品(pkgItem)
+                        延时(100)
+                    end
+                end
+            end
+        end
+    end
+end
+
+FirstUse()
+延时(1000)
+执行功能("自动清包")
