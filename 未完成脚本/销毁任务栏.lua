4333eb8c28ec50d1f058e64f80249b7f3c4b72c8
Your Name <you@example.com>
2023-06-18 21:34:43 +0800

    update | 添加销毁任务栏物品脚本

--- 未完成脚本/销毁任务栏.lua
+++ 未完成脚本/销毁任务栏.lua
@@ -7,12 +7,8 @@ local taskNum = LUA_取返回值([[
 ]])
 
 
-LUA_Call([[]])  -- 打开背包，切换到任务栏
-LUA_Call([[]]) -- 点击整理
-
 for i = 1, tonumber(taskNum) do
     local isTarget = LUA_取返回值(string.format([[
-        local targetItemName = "%s"
         local theAction,bLocked = PlayerPackage:EnumItem("quest", %d-1);
 		if theAction:GetID() ~= 0 then
 			local itemName = theAction:GetName()
@@ -26,7 +22,17 @@ for i = 1, tonumber(taskNum) do
 			end
 		end
 		return -1
-    ]], targetItemName, i))
+    ]], i))
+
+
+	local row = math.floor(i / 10)
+    local col = math.floor(i % 10)
+    if col == 0 then
+        col = 10
+    else
+        row = row + 1
+    end
+
 	if tonumber(isTarget) == 1 then
 		跨图寻路("洛阳", 360, 183)
 		延时(2000)
@@ -34,10 +40,12 @@ for i = 1, tonumber(taskNum) do
 		延时(500)
 		NPC二级对话("我想删除任务道具")
 		延时(1000)
-		LUA_Call()
+		LUA_Call(string.format([[setmetatable(_G, {__index = Packet_Env});Packet_ItemBtnClicked(%d,%d);]], row, col))  -- 右键选中物品
+		延时(500)
+		LUA_Call(string.format([[]]))  -- TODO 点击销毁按钮
+		延时(500)
+		LUA_Call([[]]) -- TODO 关闭销毁界面
 	elseif tonumber(isTarget) == 2 then
-		
+		LUA_Call(string.format([[setmetatable(_G, {__index = Packet_Env});Packet_ItemBtnClicked(%d,%d);]], row, col))  -- 右键使用棋子
 	end
-	
-	
 end
