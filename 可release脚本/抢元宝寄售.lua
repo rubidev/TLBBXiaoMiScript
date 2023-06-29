3aea2f387f8f745a7ab856e7827237d1bdf77ef8
Your Name <you@example.com>
2023-06-29 23:13:24 +0800

    update | releaseæe抢寄售元宝脚本

--- 可release脚本/抢元宝寄售.lua
+++ 可release脚本/抢元宝寄售.lua
@@ -1,23 +1,21 @@
 取出物品("金币")
-跨图寻路("洛阳", 160, 181)
+跨图寻路("洛阳", 256, 321)
 延时(1000)
-local _, npcID, _, _, _, _, _, _, _, _ = 遍历周围物品(1, "汀汀", 3)
 对话NPC("汀汀")
 延时(500)
+NPC二级对话("我想购买元宝")
+延时(1000)
 -- CommisionStall.lua
 while true do
-    NPC二级对话("购买元宝") -- TODO
-    延时(110)
     for g_grad = 0, 2 do
-        -- TODO 切换到不同类型的元宝店
         LUA_Call(string.format([[
-
-        ]], g_grad))
+            setmetatable(_G, {__index = CommisionStall_Env});
+            CommisionStall_ChangeTabIndex(%d);
+        ]], g_grad + 1))
         延时(500)
 
         -- 购买元宝
         LUA_Call(string.format([[
-            npcID = %d
             g_grad = %d
             if(g_grad<=2)then
                 -- 元宝店
@@ -25,7 +23,7 @@ while true do
 			else
 				g_Type = 2
 			end
-            for idx = 1, 3 then
+            for idx = 1, 3 do
                 -- 目前寄售的人少，无需遍历20个，只遍历前3个数据即可
                 local theAction = CommisionShop:EnumAction(idx-1);
                 if theAction:GetID() ~= 0 then
@@ -34,13 +32,13 @@ while true do
                     Clear_XSCRIPT();
                         Set_XSCRIPT_Function_Name("Buy");
                         Set_XSCRIPT_ScriptID(800116);
-                        Set_XSCRIPT_Parameter(0,tonumber(npcID));
+                        Set_XSCRIPT_Parameter(0,tonumber(191));
                         Set_XSCRIPT_Parameter(1,tonumber(g_grad));
                         Set_XSCRIPT_Parameter(2,tonumber(Sailer));
                         Set_XSCRIPT_ParamCount(3);
                     Send_XSCRIPT();
                 end
             end
-        ]], npcID, g_grad))  -- TODO g_grad为：0,1,2，分别对应寄售种类50点元宝，200点元宝，500点元宝
+        ]], g_grad))  -- TODO g_grad为：0,1,2，分别对应寄售种类50点元宝，200点元宝，500点元宝
     end
 end
