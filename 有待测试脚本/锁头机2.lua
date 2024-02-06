2b6ce9325e6160c1df8858ad178309174ece5691
hufei BRJXPC <hufei@brjx.com>
2024-02-06 17:34:39 +0800

    update | 更新锁头机第二版

--- 有待测试脚本/锁头机2.lua
+++ 有待测试脚本/锁头机2.lua
@@ -1,5 +1,8 @@
-目标名字关键字 = {}
--- 目标名字关键字 = { "至尊", "君临", "异界侠士"}
+目标名字关键字 = ''
+-- 目标名字关键字 = "至尊|君临|异界侠士"
+
+目标门派 = '武当|唐门'
+
 
 杀死敌人秒数 = 5
 
@@ -26,8 +29,38 @@ function StringSplit(str, reps)
 end
 
 
-function SearchMinHPTarget(filterTargetList)
+function SearchMinHPTarget(filterTargetList, memPaiList)
     local mainTargetInfo = LUA_取返回值(string.format([[
+        local memPaiList = {
+            [10]="少林"
+            [11]="明教"
+            [12]="丐帮"
+            [13]="武当"
+            [14]="峨嵋"
+            [15]="星宿"
+            [16]="天龙"
+            [17]="天山"
+            [18]="逍遥"
+            [19]="无门派"
+            [20]="慕容"
+            [21]="唐门"
+            [22]="鬼谷"
+            [23]="桃花岛"
+            [24]="绝情谷"
+        }
+
+        local needMenPaiList = "%s"
+        function isNeedMenPai()
+            local nMenpai = Target:GetMenPai();
+            local memPaiName = memPaiList[nMenpai+10]
+            if string.find(memPaiName, needMenPaiList) then
+                flag = true
+            else
+                flag = false
+            end
+            return flag
+        end
+
         function HavaUnbeatableBuff()
             local TARGET_BUFF_MAX = 6;
             local TARGET_IMPACT_NUM = 20; --查询时要从20个里面选出优先级最高的6个
@@ -57,16 +90,25 @@ function SearchMinHPTarget(filterTargetList)
         for i=1,2000 do
             if SetMainTargetFromList(i ,false, false) == 1 then   -- TODO
                 local playerName = Target:GetName()
-                local flag = false
+                local nameFlag = false
                 if filterTargetList ~= nil and filterTargetList ~= '' then
                     if string.find(filterTargetList, mainTargetName) then
-                        flag = true
+                        nameFlag = true
                     end
                 else
-                    flag = true
+                    nameFlag = true
                 end
 
-                if flag then
+                local menPaiFlag = false
+                if needMenPaiList ~= nil and needMenPaiList ~= '' then
+                    if isNeedMenPai() then
+                        menPaiFlag = true		
+                    end
+				else
+					menPaiFlag = true
+                end
+
+                if nameFlag and menPaiFlag then
                     local isUnbeatable = HavaUnbeatableBuff()
                     if isUnbeatable ~= 1 then
                         local RaidHP = Target:TargetFrame_Update_HP_Raid();
@@ -88,37 +130,47 @@ function SearchMinHPTarget(filterTargetList)
             end
         end
         return mainTargetID .. "|" .. mainTargetName
-    ]], filterTargetList))
+    ]], memPaiList, filterTargetList))
 
     local tmpList = StringSplit(mainTargetInfo, '|')
     local mainTargetID, mainTargetName = tmpList[1], tmpList[2]
     return mainTargetID, mainTargetName
 end
 
-while true do
-    local mainTargetID, mainTargetName = SearchMinHPTarget()
-
-    if mainTargetID ~= -1 and mainTargetName ~= -1 then
-        选中对象(mainTargetID)
-
-        local waitTime = 0
-        while true do
-            if waitTime >= 杀死敌人秒数 then
-                MentalTip("5秒仍未击杀, 切换到下一个目标")
-                break
-            end
-
-            local name, curID, x, y, curHP, tDistance, tType, tOwner, tJudge, tTitle = 遍历周围物品(5, mainTargetName, 15)
-            if curID ~= nil and curHP > 0 then
-                选中对象(mainTargetID)
-                延时(1000)
-            else
-                break
-            end
-
-            waitTime = waitTime + 1
-        end
+function main()
+    if 目标名字关键字 ~= '' and 目标门派 ~= '' then
+        MentalTip("【目标名字关键字】和【目标门派】只能选填一个, 请修改")
+        return
     end
-    延时(200)
+
+    while true do
+        local mainTargetID, mainTargetName = SearchMinHPTarget(目标名字关键字, 目标门派)
+
+        if mainTargetID ~= -1 and mainTargetName ~= -1 then
+            选中对象(mainTargetID)
+
+            local waitTime = 0
+            while true do
+                if waitTime >= 杀死敌人秒数 then
+                    MentalTip("5秒仍未击杀, 切换到下一个目标")
+                    break
+                end
+
+                local name, curID, x, y, curHP, tDistance, tType, tOwner, tJudge, tTitle = 遍历周围物品(5, mainTargetName, 15)
+                if curID ~= nil and curHP > 0 then
+                    选中对象(mainTargetID)
+                    延时(1000)
+                else
+                    break
+                end
+
+                waitTime = waitTime + 1
+            end
+        end
+        延时(200)
+    end
 end
+
+-- main ---------------------------------------------
+main()
 
